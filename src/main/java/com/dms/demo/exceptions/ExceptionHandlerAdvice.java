a53940f9e23fdaf3d76e325b45e207ada0a2ba0a
Iulian <iulian.ionut.buga@gmail.com>
2023-05-29 19:27:28 +0300

    Exeption Handler Advice add
    * an ExceptionHandlerAdvice has been added to the app.
    * the Spotify library for Maven has been added to the pom.xml.

--- src/main/java/com/dms/demo/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/com/dms/demo/exceptions/ExceptionHandlerAdvice.java
@@ -1,4 +1,46 @@
 package com.dms.demo.exceptions;
 
+import com.dms.demo.exceptions.user.UserNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
 public class ExceptionHandlerAdvice {
+
+    @Autowired
+    private final ObjectMapper objectMapper;
+
+    public ExceptionHandlerAdvice(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> userNotFoundException(UserNotFoundException userNotFoundException) {
+        return getExceptionResponse(userNotFoundException, HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<Object> getExceptionResponse(RuntimeException runtimeException, HttpStatus httpStatus) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("Message: ", runtimeException.getMessage());
+        return new ResponseEntity<>(objectToString(result), httpStatus);
+    }
+
+    private String objectToString(Object response) {
+        try {
+            objectMapper.findAndRegisterModules();
+            return objectMapper.writeValueAsString(response);
+        } catch (JsonProcessingException e) {
+            log.info("Json processing exception.");
+        }
+        return null;
+    }
 }
