ba8afc6c10c281142c58c9019d383fa016452046
Iulian <iulian.ionut.buga@gmail.com>
2023-06-07 10:18:25 +0300

    New Gender Enum with Gender Converter class
    * the method from String Utils Service has been renamed.
    * User has now two new properties, city, gender
    * registering now includes the city and gender.
    * gender enum will now be saved in the db as a string.

--- src/main/java/com/dms/demo/models/dto/auth/user/UserRegisterRequestDTO.java
+++ src/main/java/com/dms/demo/models/dto/auth/user/UserRegisterRequestDTO.java
@@ -1,7 +1,9 @@
 package com.dms.demo.models.dto.auth.user;
 
+import com.dms.demo.util.enums.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,4 +19,10 @@ public class UserRegisterRequestDTO implements Serializable {
 
     @NotBlank
     private String firstName;
+
+    @NotBlank
+    private String city;
+
+    @NotNull
+    private Gender gender;
 }
--- src/main/java/com/dms/demo/services/utils/StringUtilsService.java
+++ src/main/java/com/dms/demo/services/utils/StringUtilsService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StringUtilsService {
 
-    public String capitalizeNameAndRemoveWhiteSpaces(String name) {
+    public String capitalizeAndRemoveWhiteSpaces(String name) {
 
         return name.substring(0, 1).toUpperCase().replace(" ", "") + name.substring(1).toLowerCase().replace(" ", "");
     }
